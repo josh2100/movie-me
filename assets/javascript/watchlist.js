cf261e0af9d3b0cbfbcffdfaea0c7d66c043f1b8
joshgit <jschermann91@gmail.com>
2021-11-22 14:48:00 -0600

    delete function fixed

--- assets/javascript/watchlist.js
+++ assets/javascript/watchlist.js
@@ -1,10 +1,45 @@
 const watchlistDisplay = document.querySelector("#watchlist-display");
+let savedMovies = "";
 
-// load and display
+// Load and display
 const showWatchlist = function () {
-  let savedMovies = JSON.parse(localStorage.getItem("watchlist"));
+  // Clear old content
+  watchlistDisplay.innerHTML = "";
+  savedMovies = JSON.parse(localStorage.getItem("watchlist"));
 
-  watchlistDisplay.innerHTML = `<h1">${savedMovies}</h1>`;
+  savedMovies.forEach((element) => {
+    // Make a Paragraph Element
+    let newMovieEl = document.createElement("p");
+
+    // Add text
+    let movieText = document.createTextNode(`${element}`);
+
+    // Make button element
+    let newButtonEl = document.createElement("button");
+    newButtonEl.textContent = "Remove";
+
+    //Add it to Watchlist display
+    watchlistDisplay.appendChild(newMovieEl);
+    newMovieEl.appendChild(movieText);
+    newMovieEl.appendChild(newButtonEl);
+
+    newButtonEl.addEventListener("click", function (event) {
+      // Get text of <p>
+      let movieToDelete = this.parentElement.textContent.split("Remove");
+
+      let movieToDeleteString = movieToDelete[0];
+
+      // Find index
+      const movieIndex = savedMovies.indexOf(movieToDeleteString);
+
+      // Delete that element from array //splice??
+      savedMovies.splice(movieIndex, 1);
+      // Update local storage
+      localStorage.setItem("watchlist", JSON.stringify(savedMovies));
+      // Reload with new array
+      showWatchlist();
+    });
+  });
 };
 
 showWatchlist();
