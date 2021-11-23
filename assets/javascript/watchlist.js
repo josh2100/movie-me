9f7b497ba8aa57341995cd32de313b08140b0663
joshgit <jschermann91@gmail.com>
2021-11-23 08:58:23 -0600

    styled watchlist buttons

--- assets/javascript/watchlist.js
+++ assets/javascript/watchlist.js
@@ -1,15 +1,16 @@
 const watchlistDisplay = document.querySelector("#watchlist-display");
 let savedMovies = "";
 
-// Load and display
+// Load and display watchlist
 const showWatchlist = function () {
   // Clear old content
   watchlistDisplay.innerHTML = "";
   savedMovies = JSON.parse(localStorage.getItem("watchlist"));
 
   savedMovies.forEach((element) => {
-    // Make a Paragraph Element
-    let newMovieEl = document.createElement("p");
+    // Make an h2 Element
+    let newMovieEl = document.createElement("h2");
+    newMovieEl.classList += "level";
 
     // Add text
     let movieText = document.createTextNode(`${element}`);
@@ -17,22 +18,20 @@ const showWatchlist = function () {
     // Make button element
     let newButtonEl = document.createElement("button");
     newButtonEl.textContent = "Remove";
+    newButtonEl.classList = "button is-info";
 
-    //Add it to Watchlist display
+    // Add it to Watchlist display
     watchlistDisplay.appendChild(newMovieEl);
     newMovieEl.appendChild(movieText);
     newMovieEl.appendChild(newButtonEl);
 
     newButtonEl.addEventListener("click", function (event) {
-      // Get text of <p>
+      // Get text of <h2>
       let movieToDelete = this.parentElement.textContent.split("Remove");
-
       let movieToDeleteString = movieToDelete[0];
-
       // Find index
       const movieIndex = savedMovies.indexOf(movieToDeleteString);
-
-      // Delete that element from array //splice??
+      // Delete that element from array
       savedMovies.splice(movieIndex, 1);
       // Update local storage
       localStorage.setItem("watchlist", JSON.stringify(savedMovies));
